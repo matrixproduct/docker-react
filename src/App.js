2062627db1b2bfe60411c12aecb995ac7c27f985
matrixproduct <aossipov@gmail.com>
2023-01-09 14:48:14 +0000

    app small change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye Bye there
+          AWS hi there
         </p>
         <a
           className="App-link"
